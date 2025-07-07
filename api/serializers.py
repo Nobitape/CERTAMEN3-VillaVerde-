b19451ffe943a493c669ff4e48037517ce6f5571
Nobitape <martinxguajardo@gmail.com>
2025-07-07 01:14:23 -0400

    Solucion de error api

--- api/serializers.py
+++ api/serializers.py
@@ -2,23 +2,29 @@ from rest_framework import serializers
 from talleres.models import Taller
 
 class TallerSerializer(serializers.ModelSerializer):
-    profesor = serializers.SerializerMethodField()
-    lugar = serializers.SerializerMethodField()
-    categoria = serializers.SerializerMethodField()
+    profesor_nombre = serializers.SerializerMethodField(read_only=True)
+    lugar_nombre = serializers.SerializerMethodField(read_only=True)
+    categoria_nombre = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Taller
         fields = [
-            'id', 'titulo', 'fecha', 'duracion_horas', 'estado',
-            'observacion', 'profesor', 'lugar', 'categoria'
+            'id', 'titulo', 'fecha', 'duracion_horas',
+            'estado', 'observacion',
+            'profesor', 'profesor_nombre',
+            'lugar', 'lugar_nombre',
+            'categoria', 'categoria_nombre'
         ]
-        read_only_fields = ['estado', 'observacion', 'propuesto_por']
+        read_only_fields = ['estado', 'observacion', 'profesor_nombre', 'lugar_nombre', 'categoria_nombre']
 
-    def get_profesor(self, obj):
+    def get_profesor_nombre(self, obj):
         return obj.profesor.nombre_completo
 
-    def get_lugar(self, obj):
+    def get_lugar_nombre(self, obj):
         return obj.lugar.nombre
 
-    def get_categoria(self, obj):
+    def get_categoria_nombre(self, obj):
         return obj.categoria.nombre
+
+
+        
